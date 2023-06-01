d9f62bc2b514c0657265f4a937da51ceadeed0c5
HeineZo <aypierre30@gmail.com>
2023-06-01 23:43:11 +0200

    NEW ~ Page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,23 +12,63 @@ const { loadedOnce } = storeToRefs(weather);
 </script>
 
 <template>
-  <main v-if=loadedOnce class="w-full min-h-full flex items-center justify-center flex-col gap-10 px-5">
+  <main v-if=loadedOnce class="w-full flex items-center justify-center flex-col gap-10 mt-20 px-5">
     <MainInfo title="Météo" icon="sun" degrees="20" weather="Ensoleillé" />
     <MoreInfoContainer />
     <DayContainer />
   </main>
-  <main v-else class="w-full px-5 flex items-center justify-center" id="welcome">
-    <div class="h-full w-1/2 flex flex-col justify-center items-center relative">
-      <img width="300"
+  <main v-else class="w-full mt-20 px-5 flex items-center justify-center" id="welcome">
+    <div class="h-full w-1/2 flex flex-col justify-center items-center lg:relative absolute">
+      <img width="300" class="animate-rotate"
         src="https://cdn.jsdelivr.net/gh/mkabumattar/fluentui-emoji@latest/icons/modern/sun.svg" />
-      <img width="300" class="absolute left-px bottom-1/4"
+      <img width="300" class="absolute  left-px bottom-1/4 animate-move-x"
         src="https://cdn.jsdelivr.net/gh/mkabumattar/fluentui-emoji@latest/icons/modern/cloud.svg" />
-      <img width="300" class="absolute right-0.5 top-1/4"
+      <img width="300" class="absolute right-px top-1/4 animate-move-x" style="animation-direction: alternate-reverse"
         src="https://cdn.jsdelivr.net/gh/mkabumattar/fluentui-emoji@latest/icons/modern/cloud.svg" />
     </div>
-    <div class="h-full flex flex-col justify-center items-center gap-10">
-      <h1 class="font-black text-9xl uppercase text-center text-black">Météo du jour</h1>
-      <h2 class="font-semibold text-3xl text-gray-400">Recherchez une ville ou utilisez votre position</h2>
+    <div class="h-96 flex flex-col justify-center items-center gap-10 z-10 backdrop-blur-md">
+      <h1 class="font-black md:text-9xl text-6xl uppercase text-center text-black">Météo du jour</h1>
+      <h2 class="font-semibold md:text-3xl text-xl text-gray-400 text-center">Recherchez une ville ou utilisez votre position</h2>
+      <button class="rounded-xl bg-primary-300 px-5 py-2 font-bold hover:bg-primary-400">
+        <font-awesome-icon icon="map-marker-alt" class="mr-2" />
+        Me localiser
+      </button>
     </div>
   </main>
 </template>
+<style>
+/* Degradé bleu ciel vers blanc de haut en bas */
+#app {
+  background: linear-gradient(180deg, #c2defc 0%, #ffffff 100%);
+}
+
+</style>
+<style scoped>
+.animate-rotate {
+  animation: rotate 10s linear infinite;
+}
+
+.animate-move-x {
+  animation: move-x 3s ease-in-out infinite alternate;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes move-x {
+  from {
+    transform: translateX(-10px);
+  }
+
+  to {
+    transform: translateX(10px);
+  }
+}
+</style>
